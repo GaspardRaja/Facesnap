bfeaf3a756a06996374ba589da2bcd3a94ec3c92
GaspardRaja <gaspard.raja@gmail.com>
2022-06-08 14:05:40 +0200

    Changement structure pour services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,38 +6,6 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-	faceSnaps!: FaceSnap[];
-
 	ngOnInit(): void {
-		this.faceSnaps = [
-			{
-				title: 'Archibald', 
-				descrip: 'Mon meilleur ami depuis tout petit !',
-				dateCrea: new Date(),
-				likes: 0, 
-				img: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-				isLiked: false,
-				location: 'Paris'
-			},
-			{
-			  	title: 'Three Rock Mountain',
-			  	descrip: 'Un endroit magnifique pour les randonnées.',
-			  	dateCrea: new Date(),
-			  	likes: 0,
-				img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-				isLiked: false,
-			  	location: 'la montagne'
-			},
-			{
-			  	title: 'Un bon repas',
-			  	descrip: 'Mmmh que c\'est bon !',
-			  	dateCrea: new Date(),
-				likes: 0,
-			  	img: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-				isLiked: false
-			}
-	  
-
-		]
 	}
 }
---
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrls: ['./face-snap-list.component.scss']
+})
+export class FaceSnapListComponent implements OnInit {
+
+	faceSnaps!: FaceSnap[];
+
+  	constructor() { }
+
+  	ngOnInit(): void {
+		this.faceSnaps = [
+			{
+				title: 'Archibald', 
+				descrip: 'Mon meilleur ami depuis tout petit !',
+				dateCrea: new Date(),
+				likes: 0, 
+				img: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+				isLiked: false,
+				location: 'Paris'
+			},
+			{
+			  	title: 'Three Rock Mountain',
+			  	descrip: 'Un endroit magnifique pour les randonnées.',
+			  	dateCrea: new Date(),
+			  	likes: 0,
+				img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+				isLiked: false,
+			  	location: 'la montagne'
+			},
+			{
+			  	title: 'Un bon repas',
+			  	descrip: 'Mmmh que c\'est bon !',
+			  	dateCrea: new Date(),
+				likes: 0,
+			  	img: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+				isLiked: false
+			}
+		]
+  	}
+
+}
