5f1c885f1dd23f2ebeade3a0b55c788245bc58de
GaspardRaja <gaspard.raja@gmail.com>
2022-06-08 17:10:57 +0200

    Routing

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  	selector: 'app-landing-page',
+  	templateUrl: './landing-page.component.html',
+  	styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  	constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  	ngOnInit(): void {
+  	}
+
+  	onClickBtn(): void {
+		this.router.navigateByUrl('facesnaps');
+  	}
 
 }
