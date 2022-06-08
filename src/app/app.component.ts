7603b3efadeaf642f568997d304fb485a01127fb
GaspardRaja <gaspard.raja@gmail.com>
2022-06-08 11:26:25 +0200

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,38 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-	mySnap!: FaceSnap;
+	faceSnaps!: FaceSnap[];
 
 	ngOnInit(): void {
-		this.mySnap = {
-			title: 'Archibald', 
-			descrip: 'Mon meilleur ami depuis tout petit !',
-			dateCrea: new Date(),
-			likes: 0, 
-			img: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-			isLiked: false,
-			location: 'Paris'
-		}
+		this.faceSnaps = [
+			{
+				title: 'Archibald', 
+				descrip: 'Mon meilleur ami depuis tout petit !',
+				dateCrea: new Date(),
+				likes: 0, 
+				img: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+				isLiked: false,
+				location: 'Paris'
+			},
+			{
+			  	title: 'Three Rock Mountain',
+			  	descrip: 'Un endroit magnifique pour les randonnées.',
+			  	dateCrea: new Date(),
+			  	likes: 6,
+				img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+				isLiked: false,
+			  	location: 'la montagne'
+			},
+			{
+			  	title: 'Un bon repas',
+			  	descrip: 'Mmmh que c\'est bon !',
+			  	dateCrea: new Date(),
+				likes: 0,
+			  	img: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+				isLiked: false
+			}
+	  
+
+		]
 	}
 }
